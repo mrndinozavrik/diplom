ff094f1065c9b13d99aeec8226df60a02f010b64
mrndinozavrik <basketbollmarina5@gmail.com>
2023-06-15 22:25:27 +0300

    Initial Commit

---
+++ DiplomProekt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiplomProekt
+//
+//  Created by 마리나 on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
